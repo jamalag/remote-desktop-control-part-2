686e4abd276e8c203718061b10ac09e29088be27
AMIR ESHAQ <31240313+jamalag@users.noreply.github.com>
2022-11-13 18:10:59 -0500

    reverting to part 1 code

--- public/main.js
+++ public/main.js
@@ -6,67 +6,8 @@ const {
     Menu,
 } = require('electron')
 const path = require('path')
-
-const cors = require('cors')
-const express = require('express');
-const expressApp = express();
-
 let availableScreens
 let mainWindow
-
-const { createServer } = require('http')
-const { Server } = require('socket.io')
-
-expressApp.use(express.static(__dirname));
-
-expressApp.get('/', function (req, res, next) {
-    console.log('req path...', req.path)
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-expressApp.set('port', 5005)
-expressApp.use(cors({ origin: '*' }))
-expressApp.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-})
-
-const httpServer = createServer(expressApp)
-httpServer.listen(5005, '0.0.0.0')
-httpServer.on('error', e => console.log('error'))
-httpServer.on('listening', () => console.log('listening.....'))
-const io = new Server(httpServer, {
-    origin: '*',
-});
-
-const connections = io.of('/remote-ctrl')
-
-connections.on('connection', (socket) => {
-    console.log('connection established')
-
-    socket.on('offer', sdp => {
-        // send to electron app
-        socket.broadcast.emit('offer', sdp);
-    })
-
-    socket.on('answer', sdp => {
-        // send to the controller
-        socket.broadcast.emit('answer', sdp);
-    })
-
-    socket.on('icecandidate', icecandidate => {
-        socket.broadcast.emit('icecandidate', icecandidate);
-    })
-})
 
 const sendSelectedScreen = (item) => {
     mainWindow.webContents.send('SET_SOURCE_ID', item.id)
@@ -120,7 +61,7 @@ const createWindow = () => {
         }
     })
 
-    mainWindow.loadURL('https://2ce6-2607-fea8-bde2-400-a5d9-55c7-bff0-727c.ngrok.io/')
+    mainWindow.loadURL('http://localhost:4000/')
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
@@ -130,7 +71,6 @@ const createWindow = () => {
             types: ['screen']
             // types: ['window', 'screen']
         }).then(sources => {
-            sendSelectedScreen(sources[0])
             availableScreens = sources
             createTray()
             // for (const source of sources) {
--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import './App.css';
-import { useEffect, useRef } from 'react'
-
-import io from 'socket.io-client'
-
-const socket = io('https://2ce6-2607-fea8-bde2-400-a5d9-55c7-bff0-727c.ngrok.io/remote-ctrl')
+import { useRef } from 'react'
 
 function App() {
   const videoRef = useRef()
-
-  const rtcPeerConnection = useRef(new RTCPeerConnection({
-    'iceServers': [
-      { 'urls': 'stun:stun.services.mozilla.com' },
-      { 'urls': 'stun:stun.l.google.com:19302' },
-      { "urls": "stun:webrtconline.com:3478" },
-    ]
-  }))
 
   const handleStream = (stream) => {
     // const { width, height } = stream.getVideoTracks()[0].getSettings()
@@ -25,104 +13,29 @@ function App() {
     videoRef.current.onloadedmetadata = (e) => videoRef.current.play()
   }
 
-  const getUserMedia = async (constraints) => {
+  const getStream = async (screenId) => {
     try {
-      // we are not adding stream to the peer connection
-      // however, if I omit, connection seem not to work
-      const stream = await navigator.mediaDevices.getUserMedia(constraints)
-
-      // create an offer and send to the electron app
-      rtcPeerConnection.current.createOffer().then(sdp => {
-        rtcPeerConnection.current.setLocalDescription(sdp)
-
-        console.log('Send Offer sdp', JSON.stringify(sdp))
-        socket.emit('offer', sdp)
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: screenId,
+          }
+        }
       })
-    } catch (e) { console.log(e) }
+
+      handleStream(stream)
+
+    } catch (e) {
+      console.log(e)
+    }
   }
 
-  useEffect(() => {
-
-    const getStream = async (screenId) => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: screenId,
-            }
-          }
-        })
-
-        handleStream(stream)
-
-      } catch (e) {
-        console.log(e)
-      }
-    }
-
-    // event triggered from electron if running in an electron app & returns a screenId
-    // otherwise we call getUserMedia() for the controller
-    (window.electronAPI && window.electronAPI.getScreenId((event, screenId) => {
-      console.log('Renderer...', screenId)
-      getStream(screenId)
-    })) || getUserMedia({ video: true, audio: false })
-
-    socket.on('offer', offerSDP => {
-      console.log('Got Offer sdp ...', JSON.stringify(offerSDP))
-      rtcPeerConnection.current.setRemoteDescription(
-        new RTCSessionDescription(offerSDP)
-      ).then(() => {
-        rtcPeerConnection.current.createAnswer().then(sdp => {
-          rtcPeerConnection.current.setLocalDescription(sdp)
-          console.log('Sent Answer sdp')
-
-          // send back to the controller
-          socket.emit('answer', sdp)
-        }).catch(e => console.log(e))
-      })
-    })
-
-    socket.on('answer', answerSDP => {
-      console.log('Got Answer sdp ...', JSON.stringify(answerSDP))
-      rtcPeerConnection.current.setRemoteDescription(
-        new RTCSessionDescription(answerSDP)
-      )
-    })
-
-    socket.on('icecandidate', icecandidate => {
-      console.log('Got icecandidate ...', JSON.stringify(icecandidate))
-      rtcPeerConnection.current.addIceCandidate(
-        new RTCIceCandidate(icecandidate)
-      )
-    })
-
-    rtcPeerConnection.current.onicecandidate = (e) => {
-      console.log(e)
-
-      if (e.candidate) {
-        // send to the each other
-        socket.emit('icecandidate', e.candidate)
-      }
-    }
-
-    rtcPeerConnection.current.oniceconnectionstatechange = (e) => {
-      console.log(e)
-    }
-
-    rtcPeerConnection.current.ontrack = (e) => {
-      // when we receive tracks, 
-      // we get the video stream and assign to the video element srcObject
-      videoRef.current.srcObject = e.streams[0]
-
-      // we then call the play() method when metadata is loaded
-      // by this time media should have started to flow
-      videoRef.current.onloadedmetadata = (e) => videoRef.current.play()
-    }
-
-  }, [])
-
+  window.electronAPI.getScreenId((event, screenId) => {
+    console.log('Renderer...', screenId)
+    getStream(screenId)
+  })
   return (
     <div className="App">
       <>
